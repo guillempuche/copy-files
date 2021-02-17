0a96a7b57969c94ecb7798a3a760494803110759
Guillem <3519924+guillempuche@users.noreply.github.com>
2021-02-17 14:44:21 +0100

    instructions

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Run `node .\convertir-fitxers.js`
+Create 2 folders inside the project directory:
+* the origin folder "fitxes-originals" inside with folders that contain files that we'll copy
+* the destination folder "fitxes-modificades".
+
+If you want to change the name of the folders, change them also in the code.
+
+Run the program `node ./convertir-fitxers.js`
--- convertir-fitxers.js
+++ convertir-fitxers.js
@@ -1,18 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
-// C:\Users\Monitors\Desktop\Reptes TdR STEAM
-// fs.rename('/path/to/Afghanistan.png', '/path/to/AF.png', function(err) {
-//     if ( err ) console.log('ERROR: ' + err);
-// });
-
 const PATH_FITXES_ORIGINALS = "/fitxes-originals"
 const PATH_FITXES_MODIFICADES = "/fitxes-modificades"
-var filesNameWithSystemPath = [];
 
-// Function to get current directoriesName 
-// in directory 
-// const foldersName = fs.readdirSync(path.join(__dirname, `/${PATH_FITXES_ORIGINALS}`));
 const foldersName = fs.readdirSync(`./${PATH_FITXES_ORIGINALS}`);
 
 console.log(`Found ${foldersName.length} folders`);
@@ -28,13 +19,9 @@ foldersName.forEach(folderName => {
         const newFileName = `${folderName.slice(0, 10)}-${index + 1}-${fileName.slice(0, 15) + fileName.slice(-7)}`;
         const destination = path.join(__dirname, PATH_FITXES_MODIFICADES, newFileName)
 
-        console.log(fileNameWithPath);
-
         fs.copyFile(fileNameWithPath, destination, (err) => {
           if (err) throw err
         });
-
-
       })
     });
 })
